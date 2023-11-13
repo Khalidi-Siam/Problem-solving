b75027c3b823f57829e2d2fdfe2e34c40d8dfd83
Khalidi-Siam <113469913+Khalidi-Siam@users.noreply.github.com>
2023-11-13 17:07:55 +0600

    Create Sort an array of 0s, 1s and 2s.py

---
+++ Sort an array of 0s, 1s and 2s.py
@@ -0,0 +1,15 @@
+# https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+class Solution:
+    def sort012(self,arr,n):
+        # code here
+        new_arr = [0,0,0]
+        pos = 0
+        for i in range(n):
+            new_arr[arr[i]] += 1
+            
+        for i in range(3):
+            for j in range(new_arr[i]):
+                arr[pos] = i
+                pos += 1
+                
+        return arr
